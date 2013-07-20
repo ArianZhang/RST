dc76a201ac3c9dbc75cb7574f5f374f509850a4b
think <think@think-PC>
2013-07-20 18:05:43 +0800

    merge

--- 01 Code Resources/rst-web/src/main/java/com/azglxx/rst/rs/RootResource.java
+++ 01 Code Resources/rst-web/src/main/java/com/azglxx/rst/rs/RootResource.java
@@ -1,5 +1,9 @@
 package com.azglxx.rst.rs;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -13,14 +17,23 @@ import com.sun.jersey.spi.resource.Singleton;
 @Component
 @Path("/root")
 @Singleton
-//@Produces({ MediaType.APPLICATION_JSON })
-//@Consumes({ MediaType.APPLICATION_JSON })
-public class RootResource extends SuperRS{
+@Produces({ MediaType.APPLICATION_JSON })
+@Consumes({ MediaType.APPLICATION_JSON })
+public class RootResource extends SuperRS {
 
 	@Path("/hello")
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public Response hello() {
 		return ok("Hello!");
+	}
+
+	@Path("/hello2")
+	@GET
+	public Response hello2() {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("a", "avalue");
+		map.put("b", "bvalue");
+		return ok(map);
 	}
 }
